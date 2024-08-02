4a8e573abc096fc86fb823092af8adcfbb6feca2
ITMEAN <ly.phiminh@gmail.com>
2024-08-02 17:43:32 +0700

    update

--- src/api/request.js
+++ src/api/request.js
@@ -9,7 +9,6 @@ export default async function requestApi(endpoint, method, body, isInterceptors,
         "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
     };
 
-    // let baseURL = "http://54.151.178.255:8080/api/v1/"
     let baseURL = "https://18.140.64.235:8080/api/v1/"
 
     const instance = axios.create({ headers, baseURL });
